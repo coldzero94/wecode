9f4794d500c9400ca19dac4dabca754026beee1c
chanyoung <cyl0504@gmail.com>
2021-12-24 10:49:14 +0900

    ADD : test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+def find_even(elements):
+    if elements>51:
+        return False
+
+    elif elements>1:
+        if elements//2 == 0:
+            return elements
+
+    else:
+        return False
